157d0d2a8bdee9fa6f6c89589aa214f02daaee1b
angeleslibonati <libonatiangeles@gmail.com>
2024-11-09 10:40:56 -0300

    correcciones

--- ourHotel/src/Gestores/GestorHabitacion.java
+++ ourHotel/src/Gestores/GestorHabitacion.java
@@ -1,4 +1,5 @@
 package Gestores;
+
 import Enum.Servicio_Habitacion;
 import Clases.Habitacion;
 import Clases.Menu;
@@ -10,52 +11,46 @@ public class GestorHabitacion {
 
 
     //Funcion para cambiar estado de habitacion por check in
-    public static void cambioEstadoPorCheckIn (int numHabitacion){
+    public static void cambioEstadoPorCheckIn(int numHabitacion) {
 
         //Buscar una habitacion por numero
         //Cambia estado de LIBRE A OCUPADA
     }
 
     //Funcion check out
-    public static double cambioEstadoPorCheckOut (int numHabitacion){
+    public static double cambioEstadoPorCheckOut(int numHabitacion) {
         double costoPorConsumos = 0;
         Habitacion habitacion = new Habitacion();
 
-       // habitacion = //Busca la habitacion por numero  (devolver una HABITACION)
+        // habitacion = //Busca la habitacion por numero  (devolver una HABITACION)
         //cambia el estado de OCUPADA A LIMPIEZA
 
         //sumatoria de servicio
-        costoPorConsumos = consumosExtra(numHabitacion, habitacion);
+        costoPorConsumos = consumosExtra( habitacion);
 
         return costoPorConsumos;
     }
 
     //Funcion para saber los consumos extras. sino  tuvo devuelve valor $0
-    public static double consumosExtra (int numHabitacion, Habitacion h){
+    public static double consumosExtra(Habitacion h) {
+
         double costoPorConsumo = 0;
 
-        if (h.getNumHabitacion() == numHabitacion){
+        for (int i = 0; i < h.getServicios().size(); i++) {
 
-            for (int i = 0; i<h.getServicios().size(); i++){
+            Servicio servicio = h.getServicios().get(i);
 
-                Servicio servicio = new Servicio();
+            if (servicio.equals(Servicio_Habitacion.MASAJE) ||
+                    servicio.equals(Servicio_Habitacion.SPA) ||
+                    servicio.equals(Servicio_Habitacion.SAUNA) ||
+                    servicio.equals(Servicio_Habitacion.HIDROMASAJE) ||
+                    servicio.equals(Servicio_Habitacion.DESAYUNO) ||
+                    servicio.equals(Servicio_Habitacion.ALMUERZO_CENA) ||
+                    servicio.equals(Servicio_Habitacion.SERVICIO_BRINDIS) ||
+                    servicio.equals(Servicio_Habitacion.BEBIDA_SIN_ALCOHOL)) {
 
-                servicio = h.getServicios().get(i);
-                double costoUnitario = 0;
-
-                if (servicio.equals(Servicio_Habitacion.MASAJE) ||
-                        servicio.equals(Servicio_Habitacion.SPA) ||
-                        servicio.equals(Servicio_Habitacion.SAUNA) ||
-                        servicio.equals(Servicio_Habitacion.HIDROMASAJE) ||
-                        servicio.equals(Servicio_Habitacion.DESAYUNO) ||
-                        servicio.equals(Servicio_Habitacion.ALMUERZO_CENA) ||
-                        servicio.equals(Servicio_Habitacion.SERVICIO_BRINDIS) ||
-                        servicio.equals(Servicio_Habitacion.BEBIDA_SIN_ALCOHOL)) {
-
-                    costoUnitario = servicio.getCosto();
-
-                }
-                costoPorConsumo = costoPorConsumo + costoUnitario;
+                costoPorConsumo = costoPorConsumo + servicio.getCosto();
+                ;
             }
         }
 
@@ -64,7 +59,7 @@ public class GestorHabitacion {
 
 
     //Chequear dudosaa !!!!!!!!!!!!!
-    public static double costoPorHabitacion (String dni){
+    public static double costoPorHabitacion(String dni) {
 
         int cantidadNoches = GestorReserva.cantidadNoches(dni);
         Habitacion habitacion = new Habitacion();
